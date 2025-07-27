67a1cac4167328927b14dfa9cc664e2c56cc12d3
Veeras-code <nagasai692004@gmail.com>
2025-07-27 19:44:28 +0530

    Update run.py

--- run.py
+++ run.py
@@ -36,7 +36,7 @@ scans = (
 # Create a sidebar for navigation
 with st.sidebar:
     st.title("Navigation")
-    selected_page = st.radio("Go to", ["Home", "ChatBot"],key="Navigation_radio")
+    selected_page = st.radio("Go to", ["Home", "ChatBot"],key="page_selector")
 
 # HOME PAGE
 if selected_page == "Home":
